ea27c2b1c84e5349137eb740771456cee8dc4eb0
Tommy Webb <tommy@calyxinstitute.org>
2025-09-13 03:19:47 -0400

    Log out of SunPKCS11 on exit
    Resolves session exhaustion issues resulting from SunPKCS11 failing
    to ever properly log out or end a session, let alone call C_Finalize.
    Via reflection, we call logout, which is just as good for our purposes
    with YubiHSM 2.
    
    Issue: calyxos#3434
    Change-Id: I5e0e2c3efb441dd419cd326899c576f1546b9247

--- src/apksigner/java/com/android/apksigner/ApkSignerTool.java
+++ src/apksigner/java/com/android/apksigner/ApkSignerTool.java
@@ -73,6 +73,8 @@ public class ApkSignerTool {
     private static MessageDigest sha1 = null;
     private static MessageDigest md5 = null;
 
+    private static final List<Provider> installedProviders = new ArrayList<>();
+
     public static final int ZIP_MAGIC = 0x04034b50;
 
     public static void main(String[] params) throws Exception {
@@ -116,6 +118,18 @@ public class ApkSignerTool {
             System.err.println(e.getMessage());
             System.exit(1);
             return;
+        } finally {
+            finalizeProviders();
+        }
+    }
+
+    private static void finalizeProviders() {
+        for (Provider provider : installedProviders) {
+            try {
+                if ("sun.security.pkcs11.SunPKCS11".equals(provider.getClass().getName())) {
+                    provider.getClass().getMethod("logout").invoke(provider);
+                }
+            } catch (Exception ignored) { }
         }
     }
 
@@ -359,7 +373,7 @@ public class ApkSignerTool {
 
         // Install additional JCA Providers
         for (ProviderInstallSpec providerInstallSpec : providers) {
-            providerInstallSpec.installProvider();
+            installedProviders.add(providerInstallSpec.installProvider());
         }
 
         ApkSigner.SignerConfig sourceStampSignerConfig = null;
@@ -854,7 +868,7 @@ public class ApkSignerTool {
 
         // Install additional JCA Providers
         for (ProviderInstallSpec providerInstallSpec : providers) {
-            providerInstallSpec.installProvider();
+            installedProviders.add(providerInstallSpec.installProvider());
         }
 
         try (PasswordRetriever passwordRetriever = new PasswordRetriever()) {
@@ -1245,7 +1259,7 @@ public class ApkSignerTool {
             return (className == null) && (constructorParam == null) && (position == null);
         }
 
-        private void installProvider() throws Exception {
+        private Provider installProvider() throws Exception {
             if (className == null) {
                 throw new ParameterException(
                         "JCA Provider class name (--provider-class) must be specified");
@@ -1282,6 +1296,8 @@ public class ApkSignerTool {
             } else {
                 Security.insertProviderAt(provider, position);
             }
+
+            return provider;
         }
     }
 
